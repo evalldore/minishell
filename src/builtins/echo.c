5a6e457ba23944f9b7e67ff06898a20c50d6317b
Eliott Vallee dore <evallee-@c1r5p4.42quebec.com>
2023-11-15 16:12:00 -0500

    echo parameter

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 20:20:17 by niceguy           #+#    #+#             */
-/*   Updated: 2023/11/15 16:03:16 by evallee-         ###   ########.fr       */
+/*   Updated: 2023/11/15 16:10:37 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,21 @@
 void	ms_builtin_echo(char **args)
 {
 	bool	nl;
-	char	*param;
 
 	nl = true;
-	param = *args;
-	if (!param)
-		return ;
-	if (*param && ft_strncmp(param, "-n", ft_strlen(param)) == 0)
+	if (*args)
 	{
-		nl = false;
-		args++;
-	}
-	while (*args)
-	{
-		ft_putstr_fd(*args++, STDOUT_FILENO);
-		if (*args)
-			ft_putchar_fd(' ', STDOUT_FILENO);
+		if (*args[0] && ft_strncmp(*args, "-n", ft_strlen(*args)) == 0)
+		{
+			nl = false;
+			args++;
+		}
+		while (*args)
+		{
+			ft_putstr_fd(*args++, STDOUT_FILENO);
+			if (*args)
+				ft_putchar_fd(' ', STDOUT_FILENO);
+		}
 	}
 	if (nl)
 		ft_putchar_fd('\n', STDOUT_FILENO);
